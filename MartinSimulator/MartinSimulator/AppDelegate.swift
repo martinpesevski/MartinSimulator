c21316713f6ae3c0d0b169c2cf4c36a31a40ec19
Martin Peshevski <martin.m.pesevski@gmail.com>
2020-08-13 09:10:17 -0400

    added stuff

--- MartinSimulator/MartinSimulator/AppDelegate.swift
+++ MartinSimulator/MartinSimulator/AppDelegate.swift
@@ -17,3 +17,60 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 }
 
+/*
+Problems to remember with Sonia
+
+- Not allowing me to take a nap, punching me hard in the back to wake me up and kicking me out of the house
+- Hiding my headphones after I told her that I want to use them while I’m vacuuming
+- Saying it is her house and threatening to kick me out
+- Saying that I need to go to my home country if We divorce
+- Starting a fight over very little things like me not saying hi as soon as I enter the door even though she refuses to even kiss me, forcing me to go out and come back in to act the way she wants me to
+- Constant insults, saying that I’m fat and unattractive, that nobody could ever love me, that I am a useless human being, that I’m a disgrace
+- Getting angry and throwing things at me ( plates, glasses, vases ), made holes in the floor and the door and then made me fix them.
+- Throwing my phone multiple times to the walls and ground
+- Taking my phone and laptop and locking herself in a room to “punish” me for making a mistake
+- Saying that I am to blame in every fight and she is always perfect.
+- Threatening to take all my things if we divorce.
+- Saying that she doesn’t love me.
+- Treating me like a child
+- Expecting everything to happen her way
+- Not giving me alone time, making me feel suffocated
+- Making me feel unloved, bashing me and insulting me for everything single mistake I make, threatening to leave, saying she is better off without me.
+- Constant verbal abuse
+- Smacking on the head, flicking nose and other ways to purposely inflict pain
+- multiple punches in the back of the head while in a fit of rage
+- Purposely holding my bag in the car so I miss the metro and be late to work.
+- Threatening to get all my things and send me back to my country
+- Threatening to lie about me abusing her in court
+- Threatening to lie about the marriage
+- Calling me lazy for not finishing my app in the morning and saying she will find someone better one day
+- Forcing me to drink a slushee that the dog licked even though I said I didn’t want it
+- Purposely kept me away from gaming, even though I informed her I want to do it in advance, guilting me by saying I don’t love her if I do it.
+- Said I didn’t talk to her all evening even though I spent 3+ hours with her, part of which was talking
+- January/2020 Made me give up everything I’m paying for fun (games, going out with friends) for four months because I paid too much for my medical expenses even though I’m making 140k a year and this cost only $500.
+- February/2020 Backing out on her word that I can game and watch videos before bed. Kicking me out of the room for watching videos while she is asleep. Yanking headphones out of my ears to force me to stop watching. Saying that agreements don’t matter to her anymore and she doesn’t care.
+- April/2020 Forcing me to get a new dog even though I don’t want any more animals
+- April/2020 Constantly controlling me (cannot play dota, cannot go to bed late, cannot have coffee on weekends, cannot have the food I want)
+- April/2020 starting a fight about an ornament on a house - complaining that I don’t listen to her and she knows better than me about the US
+- April/2020 starting a fight because I walked on the street in front of a closed school (saying that I dont listen to her and she knows more than me when I told her that it is not illegal)
+- April/2020 Turning off the wifi while I’m gaming to force me to do what she wants - happened multiple times
+05/10/2020 - Punching me in the balls and saying it is on purpose, then trying to kick me again when I was on the floor and I instinctively lifted up my shin to defend myself from her kick and then she kicked me in the shin and complained why I tried to defend myself.
+- May 30/2020 threatening to send me back to my country if I don't do what she says
+- june 3/2020 threatening to send an email to facebook about my work ethic and try to get me fired. Abusing what I tell her against me (work email)
+- june 3/2020 constant fights for the past few days, treating me like shit, doesn't want to interact with me in any way, avoiding any
+- june 4/2020 hiding my eye sailene drops and saying I am addicted to them
+- june 5/2020 screaming in my face while I play Dota, threatening to divorce me because of it even though she agreed I can game for 3 hours per day. Hid all my electronics and lied that she put them outside. Locked herself in her room and did not speak to me for a long time
+- june 8/2020 informed her that I will game at a certain time, and she agreed. As soon as I started to game, she started begging for attention and threatening to start a fight. I abandoned the game for her, and went to spend time with her. She didn't want to hang out anymore. She said that it is either her or dota that I have to pick from. I felt forced to delete my game.
+- june 8/2020 made me give up a lot of my gaming time because she doesn't like my games. She doesn't care about my happiness.
+- june 10/2020 wanted to divorce me because I do not like the same things as other guys (cars, manual labor, sports)
+- june 11/2020 said I cannot have breakfast tomorrow because I ate too much today.
+- july 10/2020 lied about giving me a bj after showering
+- july 11/2020 lied about giving me a bj after showering
+- july 15/2020 lied about giving me a bj after showering
+- july 16/2020 lied about giving me a bj after showering
+- july 17/2020 lied about having sex in the morning
+- july 26/2020 said she doesn't want to get me weights, did not allow me to buy them. Said I need to wait until some holiday or special occasion, even though I needed them to work out during the lockdown.
+- july 26/2020 messed up the timer for the oven, and blamed me for it, even though I never even came close to it. started acting all crazy when I told her she probably did it herself and forgot. Called me a liar.
+- july 28/2020 calling me manipulating in response to being called controlling. we had a 5 hour fight and she made me admit to being manipulating, by calling cops, lawyers and threatening to kick me out of the house and deport me.
+- july 29/2020 did not let me game beforee bed even though we had an agreement. threatened to end the relationship.
+*/
